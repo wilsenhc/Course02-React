d5248b39fc50cf63d78cf13ec5dbbebaa752c64d
Wilsen Hernández <wilsenh95@gmail.com>
2020-09-20 17:48:30 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Main from './components/MainComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
