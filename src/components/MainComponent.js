a002654273e962065da90c5bcee6ae067d34ef69
Wilsen Hernández <wilsenh95@gmail.com>
2020-09-20 18:56:09 -0400

    React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -43,11 +44,18 @@ class Main extends Component {
       );
     };
 
+    const AboutPage = () => {
+      return (
+        <About leaders={this.state.leaders} />
+      )
+    }
+
     return (
       <div>
         <Header />
           <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={AboutPage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
