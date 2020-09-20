b580fab2461185223f91ceb13754c4902bdedc4e
Wilsen Hernández <wilsenh95@gmail.com>
2020-09-20 17:38:26 -0400

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
